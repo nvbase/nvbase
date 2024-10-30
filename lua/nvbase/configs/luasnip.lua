80e274fb78c23c9bfb461910d10daf3046e58aa2
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-10-30 22:23:57 +0700

    feat: revert

--- lua/nvbase/configs/luasnip.lua
+++ lua/nvbase/configs/luasnip.lua
@@ -3,8 +3,8 @@ require("luasnip.loaders.from_vscode").lazy_load({ exclude = vim.g.vscode_snippe
 require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.g.vscode_snippets_path or "" })
 
 -- snipmate format
--- require("luasnip.loaders.from_snipmate").load()
--- require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
+require("luasnip.loaders.from_snipmate").load()
+require("luasnip.loaders.from_snipmate").lazy_load({ paths = vim.g.snipmate_snippets_path or "" })
 
 -- lua format
 require("luasnip.loaders.from_lua").load()
