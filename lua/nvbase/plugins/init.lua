2f8980fba8cac376d50e9050e8da537701224750
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-10-30 22:48:05 +0700

    feat: checking icons

--- lua/nvbase/plugins/init.lua
+++ lua/nvbase/plugins/init.lua
@@ -9,6 +9,14 @@ return {
 	},
 
 	{
+		"nvim-tree/nvim-web-devicons",
+		opts = function()
+			dofile(vim.g.base46_cache .. "devicons")
+			return { override = require("nvchad.icons.devicons") }
+		end,
+	},
+
+	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "User FilePost",
 		opts = {
