e85db3b63b57ac08488d318e25b45b4e8555bff6
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-10-31 11:51:43 +0700

    fix: revert color indent

--- lua/nvbase/plugins/init.lua
+++ lua/nvbase/plugins/init.lua
@@ -26,19 +26,11 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "User FilePost",
+		main = "ibl",
 		opts = {
-			indent = { char = "┊", highlight = "IblChar" },
-			scope = { char = "│", highlight = "IblScopeChar" },
+			indent = { char = "┊" },
+			scope = { highlight = { "Normal" } },
 		},
-		config = function(_, opts)
-			dofile(vim.g.base46_cache .. "blankline")
-
-			local hooks = require("ibl.hooks")
-			hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-			require("ibl").setup(opts)
-
-			dofile(vim.g.base46_cache .. "blankline")
-		end,
 	},
 
 	-- file managing , picker etc
