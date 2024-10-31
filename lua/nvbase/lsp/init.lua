de08b2f90d3a57ac02fea54e2fbef10e1b2cdb1a
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-10-31 13:58:13 +0700

    chore: hide virtual_text

--- lua/nvbase/lsp/init.lua
+++ lua/nvbase/lsp/init.lua
@@ -4,7 +4,7 @@ M.diagnostic_config = function()
 	local x = vim.diagnostic.severity
 
 	vim.diagnostic.config({
-		virtual_text = { prefix = "" },
+		virtual_text = false,
 		signs = { text = { [x.ERROR] = "󰅙", [x.WARN] = "", [x.INFO] = "󰋼", [x.HINT] = "󰌵" } },
 		underline = true,
 		float = { border = "single" },
